f8c5c599d68e7461f64e56f3adcb37ba21da9d98
Sam Haldenby <sh695@cam.ac.uk>
2012-09-03 13:19:51 +0100

    Added some health messages and a health update loop

--- Programs/NewComponentEngine/HealthSys.cpp
+++ Programs/NewComponentEngine/HealthSys.cpp
@@ -6,6 +6,27 @@ template <>
 void System<HealthComp>::update(double elapsed)
 {
     std::cout << "Updating HealthComp" << std::endl;
+
+    //update all health components
+    std::map<ObjectId,HealthComp>::iterator iCom = components_.begin();
+    while(iCom!=components_.end())
+    {
+        HealthComp* currComponent = &iCom->second;
+
+        //ensure current health is in bound
+        if(currComponent->getCurrent() > currComponent->getMax())
+        {
+            currComponent->setCurrent(currComponent->getMax());
+        }
+
+        if(currComponent->getCurrent() <= 0)
+        {
+            //send an onZeroHealth message
+        }
+
+        ++iCom;
+    }
+
 //    std::cout << "Updating Sub" << std::endl;
 //    //read messages
 //    std::vector<Message>::iterator iMessage = messages_.begin();
@@ -64,6 +85,31 @@ void System<HealthComp>::deliverMessage_(Message message)
 
     //read message!
     std::string mainCmd = params[1];
+
+    if (mainCmd=="changeMaxBy")
+    {
+        targetComponent->setMax(targetComponent->getMax() + atoi(params[2].c_str()));
+    }
+
+    else if (mainCmd=="changeCurrBy")
+    {
+        targetComponent->setCurrent(targetComponent->getCurrent() + atoi(params[2].c_str()));
+    }
+
+    else if (mainCmd=="changeMaxTo")
+    {
+        targetComponent->setMax(atoi(params[2].c_str()));
+    }
+
+    else if (mainCmd=="changeCurrTo")
+    {
+        targetComponent->setCurrent(atoi(params[2].c_str()));
+    }
+
+    else if (mainCmd=="restoreToMax")
+    {
+        targetComponent->setCurrent(targetComponent->getMax());
+    }
 
 //    if(mainCmd=="change")
 //    {
